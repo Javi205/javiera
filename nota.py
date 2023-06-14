84cd5eb24455e953f79cc30c04d2dfdc7b5ccc51
Javi <javiera.a.flores06@gmail.com>
2023-06-14 10:00:05 -0400

    Agregue un msj que pregunta numero y funcion

--- nota.py
+++ nota.py
@@ -15,5 +15,14 @@ def primos(numero):
             pass
     return lista_primos
 
-print(es_primo(7))
-print(primos(7))
+
+dato = int(input("Que numero quieres ingresar: "))
+a = input("Que funcion quieres utilizar: (primera/segunda/ambas): ")
+
+if a == "primera":
+    print(es_primo(dato))
+elif a == "segunda":
+    print(primos(dato))
+else:
+    print(es_primo(dato))
+    print(primos(dato))
