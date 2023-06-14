d98411c289d9ba116c9413282ddd3f8b5229d832
Javi <javiera.a.flores06@gmail.com>
2023-06-14 10:38:50 -0400

    reordene la funcion y agregue la funcion 3 como opcion a ejecutar.

--- nota.py
+++ nota.py
@@ -15,19 +15,6 @@ def primos(numero):
             pass
     return lista_primos
 
-
-dato = int(input("Que numero quieres ingresar: "))
-a = input("Que funcion quieres utilizar: (primera/segunda/ambas): ")
-
-if a == "primera":
-    print(es_primo(dato))
-elif a == "segunda":
-    print(primos(dato))
-else:
-    print(es_primo(dato))
-    print(primos(dato))
-
-
 def des_prima(numero):
     factores_primos = []
     
@@ -42,4 +29,16 @@ def des_prima(numero):
             factores_primos.append(primo)
     
     return factores_primos
+
+dato = int(input("Que numero quieres ingresar: "))
+a = input("Que funcion quieres utilizar: (primera/segunda/tercera): ")
+
+if a == "primera":
+    print(es_primo(dato))
+elif a == "segunda":
+    print(primos(dato))
+elif a == "tercera":
+    print(des_prima(dato))
+else:
+    print("datos mal ingresados")
 
