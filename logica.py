1d65d5e85b1e85d00542baf09d4f31829f620220
Javi <javiera.a.flores06@gmail.com>
2023-06-14 13:47:36 -0400

    agregue una funcion que dice si el numero es perfecto o no

--- logica.py
+++ logica.py
@@ -1,6 +1,6 @@
 class Numero():
     def __init__(self, numero):
-        self.numero = numero
+        self.numero = int(numero)
         
     def es_par(self):
         if self.numero % 2 == 0:
@@ -29,9 +29,23 @@ class Numero():
             if self.numero % i == 0:
                 return True
         return False
+     
+    def es_perfecto(self): 
+        suma = 0
+        for i in range(1, self.numero):
+            if self.numero % i == 0:
+                suma += i
+                print(suma)
+        if suma == self.numero:
+            return True
+        else: 
+            return False
+
+
     
-a = Numero(10)
-print(a.es_impar())
-print(a.es_par())
-print(a.es_primo())
-print(a.es_compuesto())
+a = Numero(6)
+# print(a.es_impar())
+#print(a.es_par())
+#print(a.es_primo())
+# print(a.es_compuesto())
+print(a.es_perfecto())
